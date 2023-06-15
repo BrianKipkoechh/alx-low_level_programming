c755bac220d75f4134ab4641333abb5a76b540ff
Briantech30 <kipkoechb704@gmail.com>
2023-06-15 11:37:09 -0700

    tasks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
-include <stdio.h>
-int main(){
-	puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ * main - C program
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
