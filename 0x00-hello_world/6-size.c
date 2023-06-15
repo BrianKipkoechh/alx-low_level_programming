f31632add9601e05a344b3aa82fb2b5d8141998a
Briantech30 <kipkoechb704@gmail.com>
2023-06-15 12:38:17 -0700

    tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of int: %zu byte(s)\n", sizeof(int));
-	printf("Size of char: %zu byte(s)\n", sizeof(char));
-	 printf("Size of long: %zu byte(s)\n", sizeof(long));
-	 printf("Size of long long: %zu byte(s)\n", sizeof(long long));
-	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of int: %zu byte(s)\n",(unsigned long)sizeof(int));
+	printf("Size of char: %zu byte(s)\n", (unsigned long)sizeof(char));
+	 printf("Size of long: %zu byte(s)\n", (unsigned long)sizeof(long));
+	 printf("Size of long long: %zu byte(s)\n", (unsigned long)sizeof(long long));
+	printf("Size of float: %zu byte(s)\n", (unsigned long)sizeof(float));
 return (0);
 }
