988338f46a7ade4412a766d893a33226c6a39fad
Briantech30 <kipkoechb704@gmail.com>
2023-06-20 09:48:32 -0700

    tasks

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
-#include <stdio.h>
-/*
- * main.h
+#include <main.h>
+/**
+ * main - Entry point
  * Return:0
  */
 int main(void)
 {
-	putchar("hello world");
+	_putchar.c("_");
+	_putchar.c("p");
+	_putchar.c("u");
+	_putchar.c("t");
+	_putchar.c("c");
+	_putchar.c("a");
+	_putchar.c("r");
+	_putchar.c("\n");
 	return 0;
 }
